4e64927b50216970e2b6548015b59a01fde031f7
oeunkyoung <o3ozzvb@gmail.com>
2025-07-30 00:11:50 +0900

    feat: jpaAuditing 적용

--- apps/commerce-api/src/main/kotlin/com/loopers/CommerceApiApplication.kt
+++ apps/commerce-api/src/main/kotlin/com/loopers/CommerceApiApplication.kt
@@ -4,8 +4,10 @@ import jakarta.annotation.PostConstruct
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import java.util.TimeZone
 
+@EnableJpaAuditing
 @ConfigurationPropertiesScan
 @SpringBootApplication
 class CommerceApiApplication {

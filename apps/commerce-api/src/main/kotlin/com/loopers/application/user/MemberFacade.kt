c7f3893bc023e23a9b1e0a427e26be317ea3305c
oeunkyoung <o3ozzvb@gmail.com>
2025-07-30 00:38:04 +0900

    chore : 작은 수정 naming

--- apps/commerce-api/src/main/kotlin/com/loopers/application/user/MemberFacade.kt
+++ apps/commerce-api/src/main/kotlin/com/loopers/application/user/MemberFacade.kt
@@ -8,23 +8,23 @@ import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
 
 @Component
-class UserFacade(
+class MemberFacade(
     private val memberService: MemberService,
 ) {
     @Transactional
-    fun signUp(command: MemberCommand.Create): UserInfo {
+    fun signUp(command: MemberCommand.Create): MemberInfo {
         memberService.find(command.userId)?.let {
             throw CoreException(ErrorType.BAD_REQUEST, "이미 존재하는 사용자입니다. ${command.userId}")
         }
 
         return memberService.create(command)
-            .let { UserInfo.from(it) }
+            .let { MemberInfo.from(it) }
     }
 
     @Transactional
-    fun me(userId: String): UserInfo {
+    fun me(userId: String): MemberInfo {
         return memberService.find(userId)
-            ?.let { UserInfo.from(it) }
+            ?.let { MemberInfo.from(it) }
             ?: throw CoreException(ErrorType.BAD_REQUEST, "잘못된 접근입니다. 해당 사용자를 찾을 수 없습니다: $userId")
     }
 }

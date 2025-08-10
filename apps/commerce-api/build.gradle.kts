b8bdbe793e5e5d32f61844b50a34d9a3a5006b90
hubtwork <hubtwork@gmail.com>
2025-08-10 13:35:27 +0900

    :apps:commerce-api 에 :modules:redis 의존성 추가

--- apps/commerce-api/build.gradle.kts
+++ apps/commerce-api/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 dependencies {
     // add-ons
     implementation(project(":modules:jpa"))
+    implementation(project(":modules:redis"))
     implementation(project(":supports:jackson"))
     implementation(project(":supports:logging"))
     implementation(project(":supports:monitoring"))
@@ -19,4 +20,5 @@ dependencies {
 
     // test-fixtures
     testImplementation(testFixtures(project(":modules:jpa")))
+    testImplementation(testFixtures(project(":modules:redis")))
 }
